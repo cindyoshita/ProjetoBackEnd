22075f0a8569ca0fe31bfe4d90b950d64a3208aa
cindyoshita <79462846+cindyoshita@users.noreply.github.com>
2023-11-24 23:24:01 -0300

    parte 2

---
+++ parte1/script/obter_feedbacks.mjs
@@ -0,0 +1,10 @@
+import {
+    ScanCommand
+    } from "@aws-sdk/lib-dynamodb";
+    export default async (docClient, tableName) => {
+    const command = new ScanCommand({
+    TableName: tableName
+    });
+    const response = await docClient.send(command);
+    return response.Items;
+    }
