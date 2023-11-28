8e0a5d85361a0735652756d204dea97dcf8f30d2
cindyoshita <79462846+cindyoshita@users.noreply.github.com>
2023-11-28 20:41:15 -0300

    Update inserir_feedback.mjs

--- parte1/script/inserir_feedback.mjs
+++ parte1/script/inserir_feedback.mjs
@@ -1,33 +1,12 @@
-import inserir_feedback from './inserir_feedback.mjs';
-import obter_feedbacks from './obter_feedbacks.mjs';
-import obter_feedback from './obter_feedback.mjs';
+import {
+    PutCommand,
+} from "@aws-sdk/lib-dynamodb";
 
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient } from
-"@aws-sdk/lib-dynamodb";
-
-const funcoes = {
-"POST": ({docClient, tableName, payload, id}) =>
-inserir_feedback(docClient, tableName, payload, context.awsRequestId),
-"GET": ({docClient, tableName, id}) => id === "all" ?
-obter_feedbacks(docClient, tableName) : obter_feedback(docClient, tableName,
-id)
+export default async (docClient, tableName, feedback, id) => {
+    const command = new PutCommand({
+        TableName: tableName,
+            Item: {...feedback, FeedbackID: id},
+        });
+    const response = await docClient.send(command);
+    return {...feedback, id: id};
 }
-
-const client = new DynamoDBClient({});
-const docClient = DynamoDBDocumentClient.from(client);
-
-const tableName = "Feedback";
-
-export const handler = async (event, context) => {
-const {method, payload, id} = event
-
-try{
-return funcoes[method]({docClient, tableName, payload, id});
-}
-catch (e){
-console.log(event);
-return event;
-}
-
-};
